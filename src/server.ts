c9d3ab82323cf522f008a74dd2572a2bd8ab0032
amitsengupta <amitsengupta332@gmail.com>
2025-11-25 00:11:46 +0600

    Building a Server with Node.js

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import path from "path";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("server is running");
+
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "Hello Nodejs with Typescript",
+          path: req.url,
+        })
+      );
+    }
+  }
+);
+
+server.listen(5000, () => {
+  console.log(`server is running on port ${5000}`);
+});
