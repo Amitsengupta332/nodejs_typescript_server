98db691b8aa917d639df09cb3a3a272304cd1968
amitsengupta <amitsengupta332@gmail.com>
2025-11-25 23:30:11 +0600

    API Routes & the Difference Between GET and POST

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import config from "./config";
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running");
-
+    // root route
     if (req.url == "/" && req.method == "GET") {
       res.writeHead(200, { "Content-Type": "application/json" });
       res.end(
@@ -14,6 +14,38 @@ const server: Server = http.createServer(
           path: req.url,
         })
       );
+    }
+
+    // health route
+    if (req.url == "/api" && req.method == "GET") {
+    }
+
+    if (req.url == "/api/users" && req.method == "POST") {
+      // const user = {
+      //   id: 1,
+      //   name: "Amit",
+      // };
+      // res.writeHead(200, { "Content-Type": "application/json" });
+      // res.end(JSON.stringify(user));
+
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        try {
+          const parseBody = JSON.parse(body);
+          res.end(
+            JSON.stringify(parseBody)
+          );
+          // console.log()
+        } catch (error: any) {
+          console.log(error?.message);
+        }
+        // const parseBody = JSON.parse(body);
+        // console.log()
+      });
     }
   }
 );
